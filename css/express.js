989c40724dfaab6c56ac0c253882999a08921ffb
Kevin Goslar <kevin.goslar@gmail.com>
2022-11-03 06:21:04 -0500

    chore: license headers

--- css/express.js
+++ css/express.js
@@ -1,4 +1,5 @@
 // Copyright © 2022 Ory Corp
+// SPDX-License-Identifier: Apache-2.0
 
 const postcss = require("postcss")
 const postcssPresetEnv = require("postcss-preset-env")
--- src/theme/globalStyles.ts
+++ src/theme/globalStyles.ts
@@ -1,4 +1,5 @@
 // Copyright © 2022 Ory Corp
+// SPDX-License-Identifier: Apache-2.0
 
 import { createGlobalStyle } from "styled-components"
 import normalize from "./normalizeStyles"
